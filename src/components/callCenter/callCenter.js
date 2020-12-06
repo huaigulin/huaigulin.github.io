68b71d72ebde6d18b5ada03b4ca3e017b021ca40
Huaigu Lin <kevinlinhg@outlook.com>
2020-12-06 16:00:53 +0800

    update 1st project description with style change

--- src/components/callCenter/callCenter.js
+++ src/components/callCenter/callCenter.js
@@ -1,6 +1,12 @@
 import React, { useState, Fragment } from "react";
 import { makeStyles, useTheme } from "@material-ui/styles";
-import { Button, Grid, Typography, useMediaQuery } from "@material-ui/core";
+import {
+    Button,
+    Grid,
+    Link,
+    Typography,
+    useMediaQuery,
+} from "@material-ui/core";
 import { OpenInNew as OpenInNewIcon } from "@material-ui/icons";
 import Demo from "./demo";
 
@@ -30,21 +36,79 @@ export default () => {
             className={isSmScreen ? classes.backgroundSm : classes.background}
         >
             <Grid item>
-                <Typography variant="h5" color="textPrimary">
+                <Typography
+                    variant="h5"
+                    color="textPrimary"
+                    style={isSmScreen ? { marginLeft: 8, marginRight: 8 } : {}}
+                >
                     Intelligent Call Center for Banks, Proof of Concept
                 </Typography>
             </Grid>
             {displayVideo ? (
                 <Fragment>
                     <Grid item>
-                        <iframe
-                            width={isSmScreen ? "100%" : "736"}
-                            height={isSmScreen ? "315" : "480"}
-                            src="https://www.youtube.com/embed/v6TqeO2SzT0"
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        />
+                        <Grid container spacing={4} alignItems="center">
+                            <Grid item xs={12} md={6}>
+                                <iframe
+                                    style={
+                                        isSmScreen
+                                            ? { marginLeft: 8, marginRight: 8 }
+                                            : { marginLeft: 16 }
+                                    }
+                                    width="100%"
+                                    height={isSmScreen ? "315" : "480"}
+                                    src="https://www.youtube.com/embed/v6TqeO2SzT0"
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <Typography
+                                    color="textPrimary"
+                                    style={
+                                        isSmScreen
+                                            ? { marginLeft: 8, marginRight: 8 }
+                                            : { marginRight: 16 }
+                                    }
+                                >
+                                    Using <i>speech recognition</i>,{" "}
+                                    <i>text to speech</i>,{" "}
+                                    <i>intent recognition</i> and{" "}
+                                    <i>frontend animation</i> to showcase the
+                                    company's technical expertise in building an
+                                    intelligent call center
+                                </Typography>
+                                <br />
+                                <Typography color="textPrimary">
+                                    Type of Work: professional, independent
+                                </Typography>
+                                <br />
+                                <Typography color="textPrimary">
+                                    Icons:{" "}
+                                    <Link
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        href="https://streamlineicons.com/"
+                                        color="textSecondary"
+                                    >
+                                        Streamline Icons
+                                    </Link>
+                                </Typography>
+                                <br />
+                                <Typography color="textPrimary">
+                                    Speech Services:{" "}
+                                    <Link
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        href="https://azure.microsoft.com/en-us/services/cognitive-services/speech-services/"
+                                        color="textSecondary"
+                                    >
+                                        Microsoft Azure
+                                    </Link>
+                                </Typography>
+                            </Grid>
+                        </Grid>
                     </Grid>
                     <Grid item style={{ width: "100%" }}>
                         <Grid container justify="center" spacing={4}>
