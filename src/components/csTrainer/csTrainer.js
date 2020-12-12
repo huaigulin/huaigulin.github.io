931a6fd9fde40b62c4703c7371165227fbf7a509
Huaigu Lin <kevinlinhg@outlook.com>
2020-12-13 00:23:00 +0800

    new module

--- src/components/csTrainer/csTrainer.js
+++ src/components/csTrainer/csTrainer.js
@@ -1,26 +1,104 @@
 import React, { useState } from "react";
 import { makeStyles, useTheme } from "@material-ui/styles";
-import { Typography } from "@material-ui/core";
+import { Grid, Link, Typography, useMediaQuery } from "@material-ui/core";
+import { OpenInNew as OpenInNewIcon } from "@material-ui/icons";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     background: {
         height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "calc(10px + 2vmin)",
-        color: "white",
+        width: "100%",
     },
-});
+    backgroundSm: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+        width: "100%",
+    },
+}));
 
 export default () => {
     const classes = useStyles();
+    const theme = useTheme();
+    const isSmScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
     return (
-        <div className={classes.background}>
-            <h1>Customer Service AI Trainer</h1>
-            <p>Demo</p>
-        </div>
+        <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            spacing={2}
+            className={isSmScreen ? classes.backgroundSm : classes.background}
+        >
+            <Grid item>
+                <Typography variant="h5" color="textPrimary" align="center">
+                    Customer Service AI Trainer
+                </Typography>
+            </Grid>
+            <Grid item>
+                <Grid container spacing={4} alignItems="center">
+                    <Grid item xs={12} md={4}>
+                        <video
+                            src="https://personalwebsite2.blob.core.windows.net/container/customer_service_ai_trainer_demo.mp4"
+                            type="video/mp4"
+                            autoPlay
+                            loop
+                            width="100%"
+                            muted
+                            playsInline
+                            style={
+                                isSmScreen
+                                    ? { marginLeft: 8, marginRight: 8 }
+                                    : { marginLeft: 16, maxHeight: 540 }
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Typography
+                            color="textPrimary"
+                            align="center"
+                            style={
+                                isSmScreen
+                                    ? { marginLeft: 8, marginRight: 8 }
+                                    : { marginRight: 16 }
+                            }
+                        >
+                            <b>
+                                An Android native APP that trains customer
+                                service how to talk to customers.{" "}
+                                <i>Speech recognition</i> is used to recognize
+                                users' voice. A custom <i>NLP module</i> is used
+                                to mimic customers' questions, score customer
+                                services' answers and give out standard answers
+                            </b>
+                        </Typography>
+                        <br />
+                        <Typography color="textPrimary" align="center">
+                            Type of Work: <i>professional</i>,{" "}
+                            <i>independent</i>
+                        </Typography>
+                        <br />
+                        <Typography color="textPrimary" align="center">
+                            Speech Recognition:{" "}
+                            <Link
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href="https://www.xfyun.cn/services/voicedictation"
+                                color="textSecondary"
+                            >
+                                Xunfei Cloud
+                                <OpenInNewIcon
+                                    color="inherit"
+                                    fontSize="inherit"
+                                />
+                            </Link>
+                        </Typography>
+                        <br />
+                        <Typography color="textPrimary" align="center">
+                            NLP Module: Zhijue Information
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 };
