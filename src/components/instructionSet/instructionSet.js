79558525b139feb81156fffdea2233da25cfe798
Huaigu Lin <kevinlinhg@outlook.com>
2021-09-29 23:42:48 -0400

    add instruction set content

--- src/components/instructionSet/instructionSet.js
+++ src/components/instructionSet/instructionSet.js
@@ -46,6 +46,65 @@ export default () => {
                     <b>Instruction Set for Strangers</b>
                 </Typography>
             </Grid>
+            <Grid item>
+                <Grid container spacing={4} alignItems="center">
+                    <Grid item xs={12} md={7}>
+                        <iframe
+                            style={
+                                isSmScreen
+                                    ? { marginLeft: 8, marginRight: 8 }
+                                    : { marginLeft: 16 }
+                            }
+                            width="100%"
+                            height={isSmScreen ? "315" : "480"}
+                            src="https://www.youtube.com/embed/y5e17zLZ26k"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={5}>
+                        <Typography
+                            color="textPrimary"
+                            align="center"
+                            style={
+                                isSmScreen
+                                    ? { marginLeft: 8, marginRight: 8 }
+                                    : { marginRight: 16 }
+                            }
+                        >
+                            <b>
+                                How to make an installation and get strangers to
+                                interact with it <i>without using any text</i>?
+                            </b>
+                        </Typography>
+                        <br />
+                        <Typography color="textPrimary" align="center">
+                            Type of Work: <i>school project</i>, <i>team</i>
+                        </Typography>
+                        <br />
+                        <Typography color="textPrimary" align="center">
+                            Teammate: Tricia Ilena
+                        </Typography>
+                        <br />
+                        <Typography color="textPrimary" align="center">
+                            Location:{" "}
+                            <Link
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                href="https://goo.gl/maps/STaZkdQ2Fe89q62w9"
+                                color="textSecondary"
+                            >
+                                Pier I, Riverside Park South, New York, NY
+                                <OpenInNewIcon
+                                    color="inherit"
+                                    fontSize="inherit"
+                                />
+                            </Link>
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
         </Grid>
     );
 };
